095863fc3397297383d5c6b2f34d6e826102d889
Jessie Chen <yhcjessie1@gmail.com>
2020-10-08 12:06:17 -0400

    reset the game to initial start

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
 console.log('it works!')
 const Gameboxdiv=document.querySelectorAll(".game-box");
-    //console.log(Gameboxdiv);
+    console.log(Gameboxdiv);
 const Nextdiv=document.querySelector('.next-turn');
+const gameStartdiv=document.querySelector('.game-status');
 const Resetdiv=document.querySelector('.reset');
+let Activegame = true;
+let startPlayer = "O";
+const PlayerTurn= function(){
+    return `Player ${startPlayer} turn`;
+}
+let gameStart = ["", "", "", "", "", "", "", "", ""];
  
 Resetdiv.addEventListener('click',(event)=>{
     event.preventDefault();
     console.log('Reset...');
+    Activegame = true;
+    currentPlayer = "O";
+    gameStart = ["", "", "", "", "", "", "", "", ""];
+    // Nextdiv.innerHTML = currentPlayerTurn();
+    gameStartdiv.innerHTML = PlayerTurn();
+    document.querySelectorAll(".game-box").forEach((element)=>{
+        element.innerHTML = ""
+    });
    
  })
 
 
 //=================================
- //Default X to start: querySelector
+ // next-turn: querySelector
+ //game-status:querySelector
  //clickable game-box: querySelectorAll;(done)
  //click to reset; querySelector (done)
  //=================================
 
- //Active game //click a box start
+ //Start game status
+ //Active game //
+ //Default O start player:
+
+ //set
+ //************************//
+ //reset game
+  //************************//
+
+ //gameStartdiv (#game-status)
+ //display message with start player O
+ //reset all game-box to blank
+ 
+
+
  
 
  //click  for choose O or X
