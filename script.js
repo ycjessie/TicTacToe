4171fb59b283c317613b3e2e2162e81f23f07e53
Jessie Chen <yhcjessie1@gmail.com>
2020-10-08 11:00:42 -0400

    querySelector querySelectorAll for reset and gamebox

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 console.log('it works!')
-
+const Gameboxdiv=document.querySelectorAll(".game-box");
+    //console.log(Gameboxdiv);
+const Nextdiv=document.querySelector('.next-turn');
 const Resetdiv=document.querySelector('.reset');
- Resetdiv.addEventListener('click',(event)=>{
+ 
+Resetdiv.addEventListener('click',(event)=>{
     event.preventDefault();
     console.log('Reset...');
-    // let quoteText = document.getElementById("quote-txt").value;
-    // console.log(quoteText);
+   
  })
 
 
+//=================================
+ //Default X to start: querySelector
+ //clickable game-box: querySelectorAll;(done)
+ //click to reset; querySelector (done)
+ //=================================
 
+ //Active game //click a box start
+ 
 
- //click to start game X to start
- //define two players querySelector
  //click  for choose O or X
  //if one pick o then the other X
- //click a box start
+ 
  //message after each turn
  //can't click on box twice
  //stop the game when one win/lose/tie
