0a72299865b3cdb5ccfa05fbecf82e545c8ad9b6
Jessie Chen <yhcjessie1@gmail.com>
2020-10-08 10:35:13 -0400

    reset cursor with style and js

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 console.log('it works!')
 
-const Startbutton=document.querySelector('#start-btn');
- Startbutton.addEventListener('click',(event)=>{
+const Resetdiv=document.querySelector('.reset');
+ Resetdiv.addEventListener('click',(event)=>{
     event.preventDefault();
-    console.log('Starting...');
+    console.log('Reset...');
     // let quoteText = document.getElementById("quote-txt").value;
     // console.log(quoteText);
  })
+
+
+
+
+ //click to start game X to start
+ //define two players querySelector
+ //click  for choose O or X
+ //if one pick o then the other X
+ //click a box start
+ //message after each turn
+ //can't click on box twice
+ //stop the game when one win/lose/tie
+ //automatically start a new game
