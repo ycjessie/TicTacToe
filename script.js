2bb45a45d5e57b7da8e26d3d46222a2e194c951e
Jessie Chen <yhcjessie1@gmail.com>
2020-10-08 09:18:04 -0400

    size the square and change border color

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log('it works!')
+
+const Startbutton=document.querySelector('#start-btn');
+ Startbutton.addEventListener('click',(event)=>{
+    event.preventDefault();
+    console.log('Starting...');
+    // let quoteText = document.getElementById("quote-txt").value;
+    // console.log(quoteText);
+ })
