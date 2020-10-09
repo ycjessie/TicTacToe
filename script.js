eb5f9d9196d82cdb948e4476ba2d0aa9d32c7430
Jessie Chen <yhcjessie1@gmail.com>
2020-10-08 23:47:20 -0400

    use target.classList.add to store O or X and No click twice

--- script.js
+++ script.js
@@ -1,37 +1,68 @@
 console.log('it works!')
 const Gameboxdiv=document.querySelectorAll(".game-box");//allButtons
-    console.log(Gameboxdiv);
+    //console.log(Gameboxdiv);
 const GameContainerdiv=document.querySelector(".game-container");//buttonContainer
 const Nextdiv=document.querySelector('.next-turn');
 const gameStartdiv=document.querySelector('.game-status');
 const Resetdiv=document.querySelector('.reset');
 let Activegame = true;
 let startPlayer = "O";
-let otherPlayer="X";
-const winMsg= function(){
-    return `Player ${startPlayer} wins!`;
-}
 const PlayerTurn= function(){
     return `Player ${startPlayer} turn`;
 }
-
+const winMsg= function(){
+    return `Player ${startPlayer} wins!`;
+}
 let gameStart = ["", "", "", "", "", "", "", "", ""];
 //gameStartdiv.innerHTML = PlayerTurn();
 
 //***********Click box  *******************/
 GameContainerdiv.addEventListener('click',(event)=>{
     //console.log('game container: clicked');
-    //console.log(event);//mouse click works
-    event.target.innerText=startPlayer//"hello"
-     if(event.target.classList.contains('game-box')){
+    //console.log(event);//mouse click event works also show target.classList
+    //event.target.innerText=startPlayer//'HELLO' replace with startPlayer
+    let boxClick=event.target.classList[2];
+    let boxLocation=event.target.classList[1];
+    if(boxClick==="X"|| boxClick==="O"){
+        return;
+    }
+    if(event.target.innerText==='')
+        event.target.innerText=startPlayer;
+        event.target.classList.add(startPlayer)
        
-        Gameboxdiv.forEach((elm,index)=>{
-            if(elm===event.target){
-                console.log(`${index + 1} clicked`);
-            }
-        })
+        if(startPlayer==="O"){
+            startPlayer="X"; 
+            console.log("first")
+            console.log(event.target.classList);//show mouse click on a specific box
+            gameStartdiv.innerHTML = PlayerTurn();
+        }
+        else{
+            startPlayer="O"
+            startPlayer!=startPlayer
+            console.log("next")
+            console.log(event.target.classList);
+            event.target.classList.add(startPlayer)
+            gameStartdiv.innerHTML = PlayerTurn();
+        }    
+        //console.log(event)//mouse click event
+        //console.log(event.target.classList);//show mouse click on a specific box
+        console.log(`box ${cellLocation} click`);
+    
+  
 
-     }
+
+//***************show which box is click */
+    //  if(event.target.classList.contains('game-box')){
+        
+    //     Gameboxdiv.forEach((elm,index)=>{
+    //         if(elm===event.target){
+    //             console.log(`${index + 1} clicked`);
+    //         }
+    //         else{
+
+    //         }
+    //     })
+    //  }
 })
 //*********************RESET GAME *******************/
 Resetdiv.addEventListener('click',(event)=>{
